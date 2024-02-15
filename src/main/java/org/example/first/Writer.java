f07893366fcf155a4e32dc4048b09553dd984e14
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-02-15 16:41:40 +0200

    day 2

--- src/main/java/org/example/first/Writer.java
+++ src/main/java/org/example/first/Writer.java
@@ -1,15 +1,14 @@
 private void writeDownTasksResult(StopWatch methodStopWatch, List<EconomicsTask> tasks,
-        Map<TaskResultStatus, List<EconomicsTask>> results) {
-        log.info("Total task execution time: {} seconds. " +
-        "\n Results of the tasks:: {}" +
-        "\n Total tasks:: {}, Successfully completed: {}, Failed: {}" +
-        "\n Details of Failed Tasks: {}",
-        methodStopWatch.getTotalTimeSeconds(),
-        methodStopWatch.prettyPrint(),
-        tasks.size(),
-        results.get(TaskResultStatus.SUCCESS).size(),
-        results.get(TaskResultStatus.FAILED).size(),
-        results.get(TaskResultStatus.FAILED)
+                                  Map<TaskResultStatus, List<EconomicsTask>> results){
+        log.info("Total task execution time: {} seconds. "+
+                 "\n Results of the tasks:: {}"+
+                 "\n Total tasks:: {}, Successfully completed: {}, Failed: {}"+
+                 "\n Details of Failed Tasks: {}",
+                 methodStopWatch.getTotalTimeSeconds(),
+                 methodStopWatch.prettyPrint(),
+                 tasks.size(),
+                 results.get(TaskResultStatus.SUCCESS).size(),
+                 results.get(TaskResultStatus.FAILED).size(),
+                 results.get(TaskResultStatus.FAILED)
         );
 }
-
