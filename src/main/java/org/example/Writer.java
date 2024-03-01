04750a47f8d7371ade3282117caa8fb0ca44ea8d
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-03-01 11:42:48 +0200

    2nd commit

--- src/main/java/org/example/Writer.java
+++ src/main/java/org/example/Writer.java
@@ -11,10 +11,15 @@ public class Writer {
     public void writeDownTasksResult(StopWatch methodStopWatch, List<EconomicsTask> tasks,
                                      Map<TaskResultStatus, List<EconomicsTask>> results) {
         log.info("Total task execution time: {} seconds. " +
-                        "\n Results of the tasks:: {}",
+                        "\n Results of the tasks:: {}" +
+                        "\n Total tasks:: {}, Successfully completed: {}, Failed: {}" +
+                        "\n Details of Failed Tasks: {}",
                 methodStopWatch.getTotalTimeSeconds(),
                 methodStopWatch.prettyPrint(),
-                tasks.size()
+                tasks.size(),
+                results.get(TaskResultStatus.SUCCESS).size(),
+                results.get(TaskResultStatus.FAILED).size(),
+                results.get(TaskResultStatus.FAILED)
         );
     }
 }
