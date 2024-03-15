b28dc0707c81f6e4bcfb19c50e01179a183d7c98
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-03-15 15:36:54 +0200

    two

--- src/main/java/org/example/AccountsController.java
+++ src/main/java/org/example/AccountsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import static java.util.Objects.isNull;
+
 @RestController
 @RequiredArgsConstructor
 public class AccountsController {
@@ -16,7 +18,9 @@ public class AccountsController {
     @GetMapping("/{clubId}/accounts")
     public ResponseEntity<RawJson> getAccounts(@PathVariable Long clubId, @RequestParam String name) {
 
-        String result = economicService.getAccounts(clubId, name);
+        String result = isNull(name)
+                ? economicService.getAccounts(clubId)
+                : economicService.getAccounts(clubId, name);
 
         return new ResponseEntity<>(RawJson.from(result), HttpStatus.OK);
     }
