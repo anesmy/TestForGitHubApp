ec537bc27b1e824ccd2191c02e117484f6cd0e4a
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-03-22 13:19:45 +0200

    prepare

---
+++ src/main/java/org/example/HttpException.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.http.HttpStatus;
+
+public class HttpException extends RuntimeException {
+    private final HttpStatus statusCode;
+    public HttpException(String message, HttpStatus statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+}
