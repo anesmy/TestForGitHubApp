168ed69f6177ca45fdd49559414c35f642f3cbcf
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-03-29 16:14:26 +0200

    prepare for demo

---
+++ src/main/java/org/example/TaskResultStatus.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class TaskResultStatus {
+    public static final TaskResultStatus SUCCESS = new TaskResultStatus();
+    public static final TaskResultStatus FAILED = new TaskResultStatus();
+}
---
+++ src/main/java/org/example/Writer.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StopWatch;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class Writer {
+    public void writeDownTasksResult(StopWatch methodStopWatch, List<EconomicsTask> tasks,
+                                     Map<TaskResultStatus, List<EconomicsTask>> results) {
+        log.info("Total task execution time: {} seconds. " +
+                 "\n Results of the tasks:: {}" +
+                 "\n Total tasks:: {}",
+                methodStopWatch.getTotalTimeSeconds(),
+                methodStopWatch.prettyPrint(),
+                tasks.size()
+        );
+    }
+}
