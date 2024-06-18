207c02d3c9ccd5b1be999b4872ccf5d3cbc99a2b
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2024-06-18 15:57:44 +0300

    2nd

--- src/main/java/org/example/Writer.java
+++ src/main/java/org/example/Writer.java
@@ -13,11 +13,13 @@ public class Writer {
                                      Map<TaskResultStatus, List<EconomicsTask>> results) {
         log.info("Total task execution time: {} seconds. " +
                  "\n Results of the tasks:: {}" +
-                 "\n Total tasks:: {}" +
+                 "\n Total tasks:: {}, Successfully completed: {}, Failed: {}" +
                  "\n Details of Failed Tasks: {}",
                 methodStopWatch.getTotalTimeSeconds(),
                 methodStopWatch.prettyPrint(),
                 tasks.size(),
+                results.get(TaskResultStatus.SUCCESS).size(),
+                results.get(TaskResultStatus.FAILED).size(),
                 results.get(TaskResultStatus.FAILED)
         );
     }
